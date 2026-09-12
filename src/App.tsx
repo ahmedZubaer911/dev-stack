123814206d31b36cddb968acae93694189017c13
Zubaer-the-learner <zubaer.ahmed.cs@gmail.com>
2026-09-12 16:08:05 +0600

    initialize React Vite project

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-5xl font-bold text-purple-600">
+        Dev Stack
+      </h1>
+    </div>
+  )
+}
+
+export default App
